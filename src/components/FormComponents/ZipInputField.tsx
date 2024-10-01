8f7681930708fa708c2186f710f1b3d83a5fc214
Elkinssm <elkinsilvamanrique@gmail.com>
2024-09-30 23:51:05 -0500

    Fix component input

--- src/components/FormComponents/ZipInputField.tsx
+++ src/components/FormComponents/ZipInputField.tsx
@@ -1,41 +1,55 @@
 import React from "react";
 import { FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
 import InputMask from "react-input-mask";
-import { FieldErrors, FieldValues, Path, UseFormRegister } from "react-hook-form";
+import {
+  FieldErrors,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
 
 interface ZipInputProps<T extends FieldValues> {
-  mask: string;
+  label: string;
+  id: string;
   zipValue: string;
   errors: FieldErrors<T>;
   register: UseFormRegister<T>;
   handleZipChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const ZipInput = <T extends FieldValues,>({
-  mask,
+const ZipInput = <T extends FieldValues>({
+  label,
+  id,
   zipValue,
   errors,
   register,
   handleZipChange,
 }: ZipInputProps<T>) => {
-  return ( // TODO, el label, id y paceholder debe conctenar el label  debe llegar por parametros, la mascara es igual para todos los zip XXXXX o XXXXX-XXXX, debe validarse si digito 5 o 9 caracteres ... 
-    <FormControl mb={4} isInvalid={!!errors.merchZip}>
-      <FormLabel htmlFor="merchZip">Location Zip</FormLabel>
+  const mask = "99999-9999";
+  const formatZipValue = (value: string) => {
+    const numericValue = value.replace(/\D/g, ""); 
+    if (numericValue.length > 5) {
+      return numericValue.slice(0, 5) + "-" + numericValue.slice(5, 9);
+    }
+    return numericValue;
+  };
+
+  return (
+    <FormControl mb={4} isInvalid={!!errors[id]}>
+      <FormLabel htmlFor={id}>{label}</FormLabel>
       <Input
         as={InputMask}
         mask={mask}
         maskChar={null}
-        value={zipValue}
-        id="merchZip"
+        value={formatZipValue(zipValue)}
+        id={id}
         type="text"
-   //     placeholder={`Enter your ${label}`}
-        {...register("merchZip" as Path<T>)}
+        placeholder={`Enter your ${label}`}
+        {...register(id as Path<T>)}
         onChange={handleZipChange}
       />
-      {errors.merchZip && (
-        <Text color="semantic.error.DEFAULT">
-          {errors.merchZip.message as string}
-        </Text>
+      {errors[id] && (
+        <Text color="red.500">{errors[id]?.message as string}</Text>
       )}
     </FormControl>
   );
--- src/components/YourBusiness/CompanyInformationForm/CompanyInformationForm.tsx
+++ src/components/YourBusiness/CompanyInformationForm/CompanyInformationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import {
   Box,
@@ -58,18 +58,21 @@ const CompanyInformationForm: React.FC<CompanyInformationFormProps> = ({
     resolver: zodResolver(schema),
     defaultValues: formData,
   });
+  const [zipValue, setZipValue] = React.useState("");
 
-  const [zipValue, setZipValue] = useState<string>("");
-  const [mask, setMask] = useState<string>("");
-  const handleZipChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/\D/g, "");
-    setZipValue(value);
-    if (value.length > 5) {
-      setMask("99999-9999");
-    } else {
-      setMask("");
-    }
+  const handleZipChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setZipValue(event.target.value);
   };
+
+  // const handleZipChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = e.target.value.replace(/\D/g, "");
+  //   setZipValue(value);
+  //   if (value.length > 5) {
+  //     setMask("99999-9999");
+  //   } else {
+  //     setMask("");
+  //   }
+  // };
 
   const onSubmit: SubmitHandler<BusinessDataForm> = (data, event) => {
     console.log(data);
@@ -154,8 +157,9 @@ const CompanyInformationForm: React.FC<CompanyInformationFormProps> = ({
         </FormControl>
       </HStack>
       <HStack spacing={4} mb={4}>
-        <ZipInput
-          mask={mask}
+        <ZipInput<BusinessDataForm>
+          label="Location Zip"
+          id="merchZip"
           zipValue={zipValue}
           errors={errors}
           register={register}
